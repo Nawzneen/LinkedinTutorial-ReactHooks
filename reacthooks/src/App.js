bb28fdbd447a21b084a94f26b49082901fe92b4b
nawzneen <earworm90@gmail.com>
2022-10-28 00:14:55 +0300

    installing the Icon packaes and practicing hooks!

--- reacthooks/src/App.js
+++ reacthooks/src/App.js
@@ -1,10 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { FaStar } from "react-icons/fa";
+import { useState } from "react";
 
 function App() {
+  const createArray = (length) => [...Array(length)];
+  console.log(createArray);
+  function Star({ selected = false, onSelect }) {
+    return <FaStar color={selected ? "yellow" : "gray"} onClick={onSelect} />;
+  }
+  function StarRating({ starNum = 5 }) {
+    const [selectedStars, setSelectedStars] = useState(0);
+    return createArray(starNum).map((item, id) => (
+      <Star
+        key={id}
+        starNum={starNum}
+        selected={selectedStars > id}
+        onSelect={() => setSelectedStars(id + 1)}
+      />
+    ));
+  }
+
   return (
     <div className="App">
-      <h1>test</h1>
+      <StarRating starNum={5} />
     </div>
   );
 }
