b75a88f59f0c00f4e974a7481f42beac1b4b538c
nawzneen <earworm90@gmail.com>
2022-10-28 12:34:37 +0300

    Practicing useReducer

--- reacthooks/src/App.js
+++ reacthooks/src/App.js
@@ -1,9 +1,26 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { FaStar } from "react-icons/fa";
-import { useState } from "react";
+import { useState, useReducer } from "react";
 
 function App() {
+  const initialState = {
+    message: "Hei",
+  };
+  function reducer(state, action) {
+    switch (action.type) {
+      case "yell":
+        return {
+          message: "HEY!",
+        };
+      case "whisper":
+        return {
+          message: "sorry for shouting",
+        };
+    }
+  }
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   const createArray = (length) => [...Array(length)];
   console.log(createArray);
   function Star({ selected = false, onSelect }) {
@@ -11,19 +28,30 @@ function App() {
   }
   function StarRating({ starNum = 5 }) {
     const [selectedStars, setSelectedStars] = useState(0);
-    return createArray(starNum).map((item, id) => (
-      <Star
-        key={id}
-        starNum={starNum}
-        selected={selectedStars > id}
-        onSelect={() => setSelectedStars(id + 1)}
-      />
-    ));
+    return (
+      <>
+        {createArray(starNum).map((item, id) => (
+          <Star
+            key={id}
+            starNum={starNum}
+            selected={selectedStars > id}
+            onSelect={() => setSelectedStars(id + 1)}
+          />
+        ))}
+        ;
+        <p>
+          {selectedStars} of {starNum}
+        </p>
+      </>
+    );
   }
 
   return (
     <div className="App">
-      <StarRating starNum={5} />
+      <StarRating starNum={7} />
+      <h1>message: {state.message}</h1>
+      <button onClick={() => dispatch({ type: "yell" })}>YELL</button>
+      <button onClick={() => dispatch({ type: "whisper" })}>whipser</button>
     </div>
   );
 }
